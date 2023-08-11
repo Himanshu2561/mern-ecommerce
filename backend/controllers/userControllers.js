4d66cfe478075026053713a3dc4c6888668bf5ce
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-08-11 20:00:13 +0530

    admin bug

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -166,7 +166,7 @@ const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
 
   if (user) {
-    if (user._id.toString() === req.params.id.toString()) {
+    if (req.user._id.toString() === req.params.id.toString()) {
       res.status(400);
       throw new Error("You yourself cannot revoke admin as admin");
     } else {
