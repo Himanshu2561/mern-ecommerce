4ed0645e97746544ab4115c600efc6323b1d7210
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-08-13 16:14:38 +0530

    latest products update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,7 +18,7 @@ const getProducts = asyncHandler(async (req, res) => {
   const count = await Product.countDocuments({ ...keyword });
 
   const products = await Product.find({ ...keyword })
-    .sort("-updatedAt")
+    .sort("-createdAt")
     .limit(pageSize)
     .skip(pageSize * (page - 1));
 
